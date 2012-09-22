8d6e8477314067bddd0b910a798b943911aea308
Andre Oschadlin <andreoschadlin@gmail.com>
2012-09-22 15:16:50 -0700

    Project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleFi::Application.config.secret_token = '0e193e7857bb7c8b85fd85d0d96cc9850f6d141a38f9a8d157afc3e24b422bf26cdc9cb498f7aec0d3c065eada2ef23bca089c4bf82f61814799bfd365eb105d'
